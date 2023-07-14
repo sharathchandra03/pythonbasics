7c9e38ba71d58752b99ceaf1f5b612c7cefe8bbd
K.Sharath Chandra <135609871+sharathchandra03@users.noreply.github.com>
2023-07-14 10:57:23 +0530

    Add files via upload

---
+++ python-basics.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(hello)
+
+
+# In[2]:
+
+
+print("hello")
+
+
+# In[3]:
+
+
+a=int(input("enter the number:"))
+print(a)
+
+
+# In[5]:
+
+
+print(10)
+
+
+# In[6]:
+
+
+###hekkooodnjjjj
+
+
+# In[7]:
+
+
+#jdjewed
+
+
+# In[8]:
+
+
+kkdkd
+
+
+# ###JBBAKHSBHM
+# 
+
+# ###HELLOM
+
+# ### GHJKJKVNDFKJDFJ
+
+# ###### hhhdh
+
+# # jbhdudu
+
+# ## hekjs
+
+# ######## hhhhhhhh
+
+# In[9]:
+
+
+prema='hello'
+
+
+# In[10]:
+
+
+prema
+
+
+# In[11]:
+
+
+z="true"
+type(z)
+
+
+# In[14]:
+
+
+z=True
+type(z)
+
+
+# In[15]:
+
+
+y=10
+type(y)
+
+
+# In[17]:
+
+
+z,y=10,38
+z
+y
+z
+
+
+# In[18]:
+
+
+z,y=10,30
+z,y
+
+
+# In[19]:
+
+
+name="sharath"
+type(name)
+
+
+# In[24]:
+
+
+name="sharath chandra"
+print(name)
+
+
+# In[23]:
+
+
+my_age=21
+print(my_age)
+type(my_age)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
